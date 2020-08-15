b227d06bbd23e2eca0af6f4981de938c5256bb5a
Nikos Galanis <ngalanis99@gmail.com>
2020-08-15 11:23:51 +0300

    Initial commit. Let's get started!

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Game-Of-Life
+# Game-Of-Life (under construction)
+
+### Nikos' notes:
+
+So far, I have done:
+ - Main to check args (TODO: possibly wrong numbering)
+ - Read grid from file (instructions @ common_functions.c)
+ - Randomize grid to begin with
+ - Implemented rules of GoL
+ - Added a small grid just to check everything is ok (under misc/grids/)
+
+
+Assummptions:
+ - Grid is char** : '0' is dead, '1' is alive
+ - 
---
+++ mpi/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <mpi.h>
+
+
+// we are going to run as:  mpiexec [-n nproc]./gol [-s size -l loops -i input]
+int main(int argc, char** argv) {
+    int grid_size, n_loops;
+    // argument checking
+    if (!strcmp(argv[1], "-s")) {
+        grid_size = atoi(argv[2]);
+    } else {
+        printf("Run as : mpiexec [-n <n_processes>] ./gol -s <grid size> -l <maximum loops> [-i <input file>]");
+        exit(EXIT_FAILURE);
+    }
+    if (!strcmp(argv[3], "-l")) {
+        n_loops = atoi(argv[4]);
+    } else {
+        printf("Run as : mpiexec [-n <n_processes>] ./gol -s <grid size> -l <maximum loops> [-i <input file>]");
+        exit(EXIT_FAILURE);
+    }
+    bool user_input;
+    char* input_file_name;
+    // user wants an extra argument, probably it's an input file
+    if (argv[5]) {
+        if (strcmp(argv[5], "-i")) {
+            user_input = true;
+        } else {
+            printf("Run as : mpiexec [-n <n_processes>] ./gol -s <grid size> -l <maximum loops> [-i <input file>]");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        // user does not want an input file, 
+        user_input = false;
+        input_file_name = strdup(argv[6]);
+    }
+    // initialize the mpi stuff
+    int size, rank; 
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+
+    char** grid;
+    // if we have user input, read from the file and fill the grid
+    if (user_input) {
+        FILE* input = fopen(input_file_name, "r");
+        grid = load_from_file(input);
+    }
+    // else, randomize the grid
+    else {
+        srand(time(NULL));
+        grid = malloc(grid_size * sizeof(char*));
+        for (int i = 0; i < grid_size; i++) {
+            grid[i] = malloc(grid_size * sizeof(char));
+            for (int j = 0; j < grid_size; j++) {
+                grid[i][j] = rand() % 2;
+            }
+        }
+    }
+}
